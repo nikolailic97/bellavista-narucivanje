06748e502a83e418eb4b1e20c7abdefbbd0f8f11
Nikola Ilic <ilicnikola1997@hotmail.com>
2026-07-14 16:33:26 +0200

    build out folder

---
+++ out/_next/static/chunks/0w0sklf7yln4g.js
@@ -0,0 +1,6 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/0n7yqb2bhjoi1.js",
+  "static/chunks/2aj5870181v6o.js",
+  "static/chunks/0nqfr9gbru8xb.css",
+  "static/chunks/turbopack-1myss791f2upk.js"
+])
