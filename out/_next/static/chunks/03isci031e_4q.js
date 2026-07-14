8507f564ec4cc4279f9a23a5b1bbdde2c54260d1
nikolailic97 <49370412+nikolailic97@users.noreply.github.com>
2026-07-14 16:34:04 +0200

    Delete out directory

--- out/_next/static/chunks/03isci031e_4q.js
+++
@@ -1,6 +0,0 @@
-__turbopack_load_page_chunks__("/kuhinja", [
-  "static/chunks/1scvproz365e0.js",
-  "static/chunks/134c2fetj7_g3.js",
-  "static/chunks/2aj5870181v6o.js",
-  "static/chunks/turbopack-26273asqq7zyo.js"
-])
--- out/_next/static/chunks/1tpo6ykx4_ekz.js
+++
@@ -1,6 +0,0 @@
-__turbopack_load_page_chunks__("/admin", [
-  "static/chunks/0ce_bfhunnkcu.js",
-  "static/chunks/287dv36491xe3.js",
-  "static/chunks/2aj5870181v6o.js",
-  "static/chunks/turbopack-2ksi1j1cio2-v.js"
-])
